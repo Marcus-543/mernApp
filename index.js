00a7e1867a511e25798da9865836d3d9e0222465
Marcus-Linhares <marcusviniciuslinhares543@gmail.com>
2023-08-05 12:46:26 -0300

    connection mongodb atlas added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const userRoute = require('./src/routes/user.route')
 const app = express()
+const connectDatabase = require('./src/database/db')
 const port = 5000
+
+connectDatabase()
 
 app.use(express.json())
 app.use('/user', userRoute)
